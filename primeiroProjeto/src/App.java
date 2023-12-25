5ae534075b0ef4d44a3bf73099f5ab63a6cfbf26
Leticia Figueiredo <143857559+leticiaafigueiredo@users.noreply.github.com>
2023-12-25 17:33:23 -0300

    adicionando pasta com os arquivos necessarios

---
+++ primeiroProjeto/src/App.java
@@ -0,0 +1,11 @@
+//Faça um programa que leia três números inteiros quaisquer, execute a soma e mostre o resultado ao final.
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+
+        // Declarando e inicializando um array de inteiros
+        float[] numeros = new float[5];
+    }
+}
