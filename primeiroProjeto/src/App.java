cd8f866cdfca483e9b1aadf219ac2137293ef8bc
Leticia Figueiredo <143857559+leticiaafigueiredo@users.noreply.github.com>
2023-12-25 21:38:19 -0300

    programa que faz a soma de tres numeros, a partir da entrada de dados

--- primeiroProjeto/src/App.java
+++ primeiroProjeto/src/App.java
@@ -1,11 +1,29 @@
 //Faça um programa que leia três números inteiros quaisquer, execute a soma e mostre o resultado ao final.
-
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        // Declarando e inicializando um array de inteiros
-        float[] numeros = new float[5];
+        int[] numeros = new int[3];
+        int soma = 0;
+        
+        //chama a funcao Scanner(System.in) da biblioteca importada
+        Scanner scanner = new Scanner(System.in);
+
+       for(var i=0; i<3; i++){
+
+        System.out.println("Digite o numero " + i + ": ");
+
+        numeros[i] = scanner.nextInt();
+
+        soma = soma + numeros[i];
+
+       }
+
+       scanner.close();
+
+       System.out.println("A soma dos numeros e: " + soma);
+
+
     }
 }
